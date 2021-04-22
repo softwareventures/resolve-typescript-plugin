16ab79361e84abcd546a0cf8a4734a664a5cc22e
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-04-22 02:10:53 +0100

    feat: first attempt at implementing resolve-typescript-plugin

---
+++ index.ts
@@ -0,0 +1,31 @@
+import {ResolveOptions} from "webpack";
+
+type Resolver = NonNullable<ResolveOptions["resolver"]>;
+
+const pluginName = "ResolveTypescriptPlugin";
+
+export default class ResolveTypescriptPlugin {
+    public apply(resolver: Resolver): void {
+        const target = "file";
+        resolver.ensureHook(target);
+        resolver.getHook("raw-file").tapAsync(pluginName, (request, resolveContext, callback) => {
+            const path = request.path && request.path.replace(/\.js$/, ".ts");
+            if (path === request.path) {
+                callback();
+            } else {
+                resolver.doResolve(
+                    target,
+                    {
+                        ...request,
+                        path,
+                        relativePath:
+                            request.relativePath && request.relativePath.replace(/\.js$/, ".ts")
+                    },
+                    `using path: ${path}`,
+                    resolveContext,
+                    callback
+                );
+            }
+        });
+    }
+}
