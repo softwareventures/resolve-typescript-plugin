69f7163449fc31b091b0a2feba36308b21d9c3ed
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-04-23 12:24:50 +0100

    feat: resolve .tsx as well as .ts

--- index.ts
+++ index.ts
@@ -8,26 +8,29 @@ export default class ResolveTypescriptPlugin {
     public apply(resolver: Resolver): void {
         const target = resolver.ensureHook("file");
         resolver.getHook("raw-file").tapAsync(pluginName, (request, resolveContext, callback) => {
-            if (!request.path || request.path.split(/[\\/]/).indexOf("node_modules") >= 0) {
+            if (!request.path || request.path.match(/(^|[\\/])node_modules($|[\\/])/)) {
                 return callback();
             }
 
-            const path = request.path.replace(/\.js$/, ".ts");
+            const path = request.path.replace(/\.js$/, "");
             if (path === request.path) {
                 callback();
             } else {
-                resolver.doResolve(
-                    target,
-                    {
-                        ...request,
-                        path,
-                        relativePath:
-                            request.relativePath && request.relativePath.replace(/\.js$/, ".ts")
-                    },
-                    `using path: ${path}`,
-                    resolveContext,
-                    callback
-                );
+                for (const extension of [".ts", ".tsx"]) {
+                    resolver.doResolve(
+                        target,
+                        {
+                            ...request,
+                            path: `${path}${extension}`,
+                            relativePath:
+                                request.relativePath &&
+                                request.relativePath.replace(/\.js$/, extension)
+                        },
+                        `using path: ${path}`,
+                        resolveContext,
+                        callback
+                    );
+                }
             }
         });
     }
