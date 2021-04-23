75b8149ef1177bde15815c7e8d43de929f386cb2
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-04-23 12:41:25 +0100

    fix: fix incorrect handling of multiple file extensions

--- index.ts
+++ index.ts
@@ -7,31 +7,33 @@ const pluginName = "ResolveTypescriptPlugin";
 export default class ResolveTypescriptPlugin {
     public apply(resolver: Resolver): void {
         const target = resolver.ensureHook("file");
-        resolver.getHook("raw-file").tapAsync(pluginName, (request, resolveContext, callback) => {
-            if (!request.path || request.path.match(/(^|[\\/])node_modules($|[\\/])/)) {
-                return callback();
-            }
+        for (const extension of [".ts", ".tsx"]) {
+            resolver
+                .getHook("raw-file")
+                .tapAsync(pluginName, (request, resolveContext, callback) => {
+                    if (!request.path || request.path.match(/(^|[\\/])node_modules($|[\\/])/)) {
+                        return callback();
+                    }
 
-            const path = request.path.replace(/\.js$/, "");
-            if (path === request.path) {
-                callback();
-            } else {
-                for (const extension of [".ts", ".tsx"]) {
-                    resolver.doResolve(
-                        target,
-                        {
-                            ...request,
-                            path: `${path}${extension}`,
-                            relativePath:
-                                request.relativePath &&
-                                request.relativePath.replace(/\.js$/, extension)
-                        },
-                        `using path: ${path}`,
-                        resolveContext,
-                        callback
-                    );
-                }
-            }
-        });
+                    const path = request.path.replace(/\.js$/, extension);
+                    if (path === request.path) {
+                        callback();
+                    } else {
+                        resolver.doResolve(
+                            target,
+                            {
+                                ...request,
+                                path,
+                                relativePath:
+                                    request.relativePath &&
+                                    request.relativePath.replace(/\.js$/, extension)
+                            },
+                            `using path: ${path}`,
+                            resolveContext,
+                            callback
+                        );
+                    }
+                });
+        }
     }
 }
