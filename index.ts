04e58b42833e0c712af63103c4f9bc7499194341
Jacob Mischka <jacobmischka@gmail.com>
2021-07-03 05:51:22 -0500

    feat: Add includeNodeModules to opt into resolving in node_modules (#10)

--- index.ts
+++ index.ts
@@ -4,14 +4,32 @@ type Resolver = NonNullable<ResolveOptions["resolver"]>;
 
 const pluginName = "ResolveTypescriptPlugin";
 
+export interface ResolveTypescriptPluginOptions {
+    includeNodeModules?: boolean;
+}
+
 export default class ResolveTypescriptPlugin {
+    private static defaultOptions: ResolveTypescriptPluginOptions = {
+        includeNodeModules: false
+    };
+
+    private options: ResolveTypescriptPluginOptions;
+
+    public constructor(options: ResolveTypescriptPluginOptions = {}) {
+        this.options = {...ResolveTypescriptPlugin.defaultOptions, ...options};
+    }
+
     public apply(resolver: Resolver): void {
         const target = resolver.ensureHook("file");
         for (const extension of [".ts", ".tsx"]) {
             resolver
                 .getHook("raw-file")
                 .tapAsync(pluginName, (request, resolveContext, callback) => {
-                    if (!request.path || request.path.match(/(^|[\\/])node_modules($|[\\/])/)) {
+                    if (
+                        !request.path ||
+                        (!this.options.includeNodeModules &&
+                            request.path.match(/(^|[\\/])node_modules($|[\\/])/))
+                    ) {
                         return callback();
                     }
 
